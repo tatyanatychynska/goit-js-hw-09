f6ad5e8f2286cb83f8eded7ce1632f444725d5f5
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-03-10 13:31:02 +0100

    Form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,43 @@
-console.log('Form');
+
+let formData = {
+    email:"",
+    message: "",
+};
+
+const storageKey = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', (e) => {
+
+    formData[e.target.name] = e.target.value.trim();
+
+    const zip = JSON.stringify(formData);
+    localStorage.setItem(storageKey, zip);
+});
+document.addEventListener('DOMContentLoaded', e => {
+    const zip = localStorage.getItem(storageKey);
+    if (zip !== null) {
+        formData = JSON.parse(zip);
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    }
+});
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formData.email === '' || formData.message === '') {
+        alert('Fill please all fields');
+        return;
+    }
+    console.log(formData);
+
+    form.reset();
+    localStorage.removeItem(storageKey);
+        
+    formData = {
+        email:"",
+        message: "",
+    };
+
+});
+
